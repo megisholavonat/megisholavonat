d8d97d64b80559f6d89b170ad670d537fe907b6d
JamDon2 <25279396+JamDon2@users.noreply.github.com>
2025-11-24 00:37:05 +0100

    feat(api): clean up config

--- apps/api/api/core/config.py
+++ apps/api/api/core/config.py
@@ -9,14 +9,12 @@ class Settings(BaseSettings):
     SOCKS5_PROXY_USERNAME: str | None = None
     SOCKS5_PROXY_PASSWORD: str | None = None
     SOCKS5_PROXY_ENABLE: bool = False
-    GRAPHQL_ENDPOINT: str = ""
-    POSTHOG_KEY: str = ""
+    GRAPHQL_ENDPOINT: str
+    POSTHOG_KEY: str | None = None
 
     # Caching Constants
     CACHE_DURATION: int = 15 * 60  # 15 minutes
     MAX_STALE_DATA_AGE: int = 15 * 60 * 1000  # 15 minutes in ms
-    REVALIDATE_DURATION: int = 60  # 60 seconds
-    REVALIDATE_LOCK_DURATION: int = 30  # 30 seconds
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
