d33f7c2ffbe6bf31a5908e40c91e991d44f5c08e
markgyoni <markgyoni@gmail.com>
2025-12-20 13:44:30 +0100

    feat: tile cache server

--- apps/web/src/env/client.ts
+++ apps/web/src/env/client.ts
@@ -1,9 +1,13 @@
 import { createEnv } from "@t3-oss/env-nextjs";
-// import { z } from "zod";
+import { z } from "zod";
 
 export const env = createEnv({
-    client: {},
-    runtimeEnv: {},
+    client: {
+        NEXT_PUBLIC_TILE_CACHE_URL: z.url().optional(),
+    },
+    runtimeEnv: {
+        NEXT_PUBLIC_TILE_CACHE_URL: process.env.NEXT_PUBLIC_TILE_CACHE_URL,
+    },
     // Skip validation during build if needed
     skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
